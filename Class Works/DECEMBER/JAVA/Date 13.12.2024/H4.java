dfd04600fc6463e7a002cf50e95469f39422da3b
Coder-Amlan-for-College <nayakamlan188@gmail.com>
2024-12-13 17:55:28 +0530

    Add files via upload

---
+++ Class Works/DECEMBER/JAVA/Date 13.12.2024/H4.java
@@ -0,0 +1,15 @@
+
+public class H4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean flag =true;
+        Boolean obj = flag;//Autoboxing
+        System.out.println("Boolean Object: "+obj);
+        boolean flag1 = false;
+        Boolean obj1 = new Boolean(flag1);//Using Constructor
+        System.out.println("Boolean Object: "+obj1);
+
+	}
+
+}
---
+++ Class Works/DECEMBER/JAVA/Date 13.12.2024/H5.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class H5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Enter the String: ");
+       String str = sc.nextLine();
+       Integer obj = Integer.valueOf(str);
+       System.out.println("Integer Object: "+obj);
+	}
+
+}
